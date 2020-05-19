b172d2b8507c42dd11c92d538bbde0df0935eff4
Parth <parth.gupta@iauro.com>
2020-05-19 17:54:15 +0530

    Initial commit

---
+++ pages/home.js
@@ -0,0 +1,6 @@
+const Home = ()=>(
+    <div>
+        <p>Home Page</p>
+    </div>
+);
+export default Home;
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import { NavLink, Router } from "react-router-dom";
+
+const Index = () => (
+    <div>
+      <p style={{color:"red",fontSize:20}}>Take me out of this boredom</p>
+    </div>
+);
+export default Index
